6703816c000199adaae49361c0a2e7a6d85071b7
shubh1410 <shubhamdarokar1410@gmail.com>
2019-07-08 10:59:28 +0530

    Iris ISO Validation

--- src/Validator.java
+++ src/Validator.java
@@ -11,7 +11,8 @@ public class Validator {
         Base64.Decoder decoder = Base64.getDecoder();
         String dStr = new String(decoder.decode(record.biometric_record));
         String str=bytesToHex(dStr.getBytes());
-        if(isValidFormat(str) && isValidVersion(str))
+        System.out.println(str);
+        if(isValidFormat(str) && isValidVersion(str) && isValidLength(str) && isValidIrisRepNumber(str) && isValidNumOfEyes(str))
             System.out.println("VALID");
         else
             System.out.print("INVALID");
@@ -27,6 +28,42 @@ public class Validator {
     private static boolean isValidFormat(String str) {
         if(str.charAt(index++)=='4' && str.charAt(index++)=='9' && str.charAt(index++)=='4' && str.charAt(index++)=='9' && str.charAt(index++)=='5'
         && str.charAt(index++)=='2' && str.charAt(index++)=='0' && str.charAt(index++)=='0')
+            return true;
+        return false;
+    }
+
+    private static boolean isValidLength(String str){
+        StringBuilder str1=new StringBuilder();
+        for(int i=0;i<8;i++){
+            str1.append(str.charAt(index++));
+        }
+        long temp=Long.parseLong(str1.toString(),16);
+        //System.out.println(temp);
+        if(temp>=69 && temp<=(4294967295L))
+            return true;
+        return false;
+    }
+
+    private static boolean isValidIrisRepNumber(String str){
+        StringBuilder str1=new StringBuilder();
+        for(int i=0;i<4;i++){
+            str1.append(str.charAt(index++));
+        }
+        long temp=Long.parseLong(str1.toString(),16);
+        //System.out.println(temp);
+        if(temp>=1 && temp<=65535)
+            return true;
+        return false;
+    }
+
+    private static boolean isValidNumOfEyes(String str){
+        StringBuilder str1=new StringBuilder();
+        for(int i=0;i<2;i++){
+            str1.append(str.charAt(index++));
+        }
+        long temp=Long.parseLong(str1.toString(),16);
+        System.out.println(temp);
+        if(temp>=0 && temp<=2)
             return true;
         return false;
     }
